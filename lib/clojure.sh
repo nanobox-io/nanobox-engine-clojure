061bf04fb1947072f60dd377119d913dccf6ce4b
Braxton Huggins <braxton@pagodabox.com>
2015-11-24 11:48:31 -0700

    refactor for nos and overlays

---
+++ lib/clojure.sh
@@ -0,0 +1,5 @@
+# -*- mode: bash; tab-width: 2; -*-
+# vim: ts=2 sw=2 ft=bash noet
+
+. ${engine_lib_dir}/java.sh
+. ${engine_lib_dir}/clojure/general.sh
---
+++ tests/sniff_test.sh
@@ -0,0 +1,16 @@
+echo running tests for clojure
+UUID=$(cat /proc/sys/kernel/random/uuid)
+
+pass "Unable to start the $VERSION container" docker run --privileged=true -d --name $UUID nanobox/build-clojure sleep 365d
+
+defer docker kill $UUID
+
+pass "Unable to create code folder" docker exec $UUID mkdir -p /opt/code
+
+fail "Detected something when there shouldn't be anything" docker exec $UUID bash -c "cd /opt/engines/clojure/bin; ./sniff /opt/code"
+
+pass "unable to remove code folder" docker exec $UUID rm -rf /opt/code
+
+pass "Failed to copy test project" docker exec $UUID cp -r /opt/tests/sample-clojure/src /opt/code
+
+pass "Failed to detect" docker exec $UUID bash -c "cd /opt/engines/clojure/bin; ./sniff /opt/code"
