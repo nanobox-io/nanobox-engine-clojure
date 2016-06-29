6ef434e2e39084a2eb5c22278c6bf3a850a99f2d
Braxton Huggins <braxton@pagodabox.com>
2016-06-29 08:53:04 -0600

    Initial commit of changes for refactor

--- lib/clojure/general.sh
+++ lib/clojure/general.sh
@@ -1,38 +1,35 @@
 # -*- mode: bash; tab-width: 2; -*-
 # vim: ts=2 sw=2 ft=bash noet
 
-clojure_create_boxfile() {
+create_boxfile() {
   nos_template \
     "boxfile.mustache" \
     "-" \
-    "$(clojure_boxfile_payload)"
+    "$(boxfile_payload)"
 }
 
-clojure_boxfile_payload() {
+boxfile_payload() {
     cat <<-END
-{
-  "has_bower": $(nodejs_has_bower)
-}
 END
 }
 
-clojure_runtime() {
+runtime() {
   echo "$(java_condensed_runtime)-clojure"
 }
 
-clojure_install_runtime() {
-  nos_install "$(clojure_runtime)"
+install_runtime() {
+  nos_install "$(runtime)"
 }
 
-clojure_install_leiningen() {
+install_leiningen() {
   nos_install "leiningen"
 }
 
-clojure_lein_uberjar() {
+lein_uberjar() {
   [[ -f $(code_dir)/bin/build ]] && return
   [[ "$(grep -c :uberjar-name $(code_dir)/project.clj)" -ge 1 ]] && (cd $(code_dir); run_subprocess "lein uberjar" "lein uberjar")
 }
 
-clojure_custom_build_script() {
+custom_build_script() {
   [[ -f $(code_dir)/bin/build ]] && (cd $(code_dir); run_subprocess "build" "bin/build")
 }
